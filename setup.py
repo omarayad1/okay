5507ee0c2cf8374721b0ca99ab465647dd29370b
omarayad1 <omarayad1@gmail.com>
2014-11-15 21:14:15 +0200

    fixed few bugs, package now installs correctly

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup
 
-setup(name='okay_lang',
+setup(name='okay',
       version='0.1',
-      description='Cause everything is okay',
-      url='http://github.com/omarayad1/the-okay-lang',
+      description='Everything is just okay',
+      url='http://github.com/omarayad1/okay',
       author='Omar Ayad',
       author_email='omarayad1@gmail.com',
+      scripts=['bin/okay'],
       license='MIT',
-      packages=['the-okay-lang'],
+      packages=['okay'],
       zip_safe=False)
